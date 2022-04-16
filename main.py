34f3f0987c52993f196955a764e8ce8e3a37b40f
0x00 <101659928+0x00Nu11@users.noreply.github.com>
2022-04-17 02:14:39 +0300

    +can download episodes from series

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from src.defs import *
+display = Display(visible=0, size=(800, 600))
+display.start()
 while True:
     try:
         operatingSystem=checkPrefs()
@@ -7,8 +9,9 @@ while True:
         if not movies:
             print('no results.')
         else:
-            link=pickMovie(movies)
-            getDownloadLink(operatingSystem, link)
+            link, title=pickMovie(movies)
+            checkMediaType(operatingSystem, link, title)
     except(KeyboardInterrupt):
         print('\n|_--> quitting...')
         break
+display.stop()
--- src/defs.py
+++ src/defs.py
@@ -2,6 +2,9 @@ from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
 from selenium import webdriver
 from selenium.webdriver import ActionChains
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import TimeoutException
+from pyvirtualdisplay import Display
 import requests, json, time, sys
 
 useragent=UserAgent().chrome
@@ -43,22 +46,25 @@ def searchSite(query):
     return moviesList
 
 def makeURL(query):
-    return "".join([i for i in query if i.isalpha() or i.isdigit() or i==' ']).rstrip()
+    return "".join([i.replace(' ', '-') for i in str(query).lower() if i.isalpha() or i.isdigit() or i==' ']).rstrip()
 
 def pickMovie(movies):
     for number, title, rating, link in movies:
         print(f'ID: {number}\ntitle: {title}\nrating: {rating}\n|')
     choice=int(input('|_(choose by ID)-->> '))
     link=(movies[choice-1])[3]
-    return link
+    title=(movies[choice-1])[1]
+    return link, makeURL(str(title))
 
 def setupSelenium(operatingSystem):
     chromeOptions=webdriver.ChromeOptions()
     chromePrefs={}
+    chromePrefs["profile.managed_default_content_settings.images"] = 2
+    chromePrefs["profile.default_content_settings.images"] = 2
     chromeOptions.add_experimental_option("prefs", chromePrefs)
     chromeOptions.add_argument('--incognito')
-    #chromeOptions.add_argument('--headless')
-    #chromeOptions.add_argument('--log-level=OFF')
+    chromeOptions.add_argument('--log-level=OFF')
+    chromeOptions.add_argument('--disable-extensions')
     chromeOptions.add_experimental_option('excludeSwitches', ['enable-logging'])
     if operatingSystem=='linux':
         driverBinary=f'src/chromedriver'
@@ -72,11 +78,11 @@ def setupSelenium(operatingSystem):
 def chooseResolution(attribs):
     count=1
     for quality, resolution, size, button in attribs:
-        print(f'{count}- {resolution}, size: {size}')
+        print(f'{count}- quality: {str(quality).strip()}, resolution: {str(resolution).strip()}, size: {str(size).strip()}')
         count+=1
     try:
         chosenResolution=int(input('|_(choose by number)-->> '))
-        if chosenResolution<=len(attribs)-1:
+        if chosenResolution<=len(attribs):
             return chosenResolution
         else:
             print('invalid option')
@@ -85,11 +91,86 @@ def chooseResolution(attribs):
         print('invalid option.')
         chooseResolution(attribs)
 
-def getDownloadLink(operatingSystem, link):
+def chooseSeason(seasons):
+    try:
+        print(f'this series has {len(seasons)} seasons.\npick a season:')
+        for i in range(len(seasons)):
+            print(f'|Season {i+1}')
+        chosenSeason=int(input(f'\n|_(choose by number)-->> '))
+        if chosenSeason<=len(seasons):
+            return chosenSeason
+        else:
+            print('invalid option')
+            chooseSeason(seasons)
+    except:
+        print('invalid option.')
+        chooseSeason(seasons)
+
+def checkSeasons(link):
+    seasons={}
+    text=requests.get(link, headers=headers).text
+    soup=BeautifulSoup(text, 'html.parser')
+    box=soup.find('div', class_='contents movies_small')
+    elements=box.find_all('a')
+    count=len(elements)
+    for element in elements:
+        url=element['href']
+        season={count:str(url)}
+        seasons.update(season)
+        count-=1
+    return seasons
+
+def checkEpisodes(chosenSeason):
+    episodes={}
+    text=requests.get(chosenSeason, headers=headers).text
+    soup=BeautifulSoup(text, 'html.parser')
+    box=soup.find('div', class_='movies_small')
+    elements=box.find_all('a')
+    count=len(elements)
+    for element in elements:
+        url=element['href']
+        episode={count:str(url)}
+        episodes.update(episode)
+        count-=1
+    return episodes
+
+def chooseEpisode(episodes):
+    try:
+        print(f'this season has {len(episodes)} episodes.\npick an episode:')
+        for i in range(len(episodes)):
+            print(f'|Episode {i+1}')
+        chosenEpisode=int(input(f'\n|_(choose by number)-->> '))
+        if chosenEpisode<=len(episodes):
+            return chosenEpisode
+        else:
+            print('invalid option')
+            chooseEpisode(episodes)
+    except:
+        print('invalid option.')
+        chooseEpisode(episodes)
+
+def checkMediaType(operatingSystem, link, title):
+    print('checking media type...')
+    if 'series' in str(link):
+        print(f'series: {title}')
+        seasons=checkSeasons(link)
+        chosenSeason=chooseSeason(seasons)
+        chosenSeason=seasons.get(chosenSeason)
+        episodes=checkEpisodes(chosenSeason)
+        chosenEpisode=chooseEpisode(episodes)
+        chosenEpisode=episodes.get(chosenEpisode)
+        getDownloadLink(operatingSystem, chosenEpisode, title)
+    else:
+        print(f'movie: {title}')
+        getDownloadLink(operatingSystem, link, title)
+
+def getDownloadLink(operatingSystem, link, title):
     print('getting available resolutions, this will take a while depending on your internet speed.')
     driver=setupSelenium(operatingSystem)
     text=requests.get(link, headers=headers).text
     driver.get(link)
+    time.sleep(3)
+    driver.execute_script('window.stop();')
     soup=BeautifulSoup(text, 'html.parser')
     infoTable=soup.find('tbody')
     infoHeaders=infoTable.find_all('tr')
@@ -104,20 +185,41 @@ def getDownloadLink(operatingSystem, link):
         attribs.append([quality, resolution, size, buttons])
     x=chooseResolution(attribs)
     downloadURL=''
+    wgetDownload=''
     while downloadURL=='':
-        time.sleep(1)
-        driver.execute_script("window.stop();")
-        time.sleep(1)
-        currentURL=driver.current_url
-        if 'egybest' in str(currentURL):
-            downloadURL=manageRedirects(driver, currentURL, x)
-        else:
-            driver.close()
-        driver.switch_to.window(driver.window_handles[len(driver.window_handles)-1])
-    wgetDownload=fetchDownload(driver, downloadURL)
-    print(wgetDownload)
+        driver.set_page_load_timeout(3)
+        try:
+            time.sleep(1)
+            currentURL=driver.current_url
+            if 'egybest' in str(currentURL):
+                downloadURL=manageRedirects(driver, currentURL, x, title)
+            else:
+                print('closing ads...')
+                driver.close()
+            driver.switch_to.window(driver.window_handles[len(driver.window_handles)-1])
+            time.sleep(1)
+        except(TimeoutException):
+            driver.execute_script('window.stop();')
+            continue
+    while wgetDownload=='':
+        driver.set_page_load_timeout(5)
+        try:
+            time.sleep(1)
+            currentURL=driver.current_url
+            if 'egybest' in str(currentURL):
+                wgetDownload=fetchDownload(driver, downloadURL, currentURL)
+            else:
+                print('closing ads...')
+                driver.close()
+            driver.switch_to.window(driver.window_handles[len(driver.window_handles)-1])
+            time.sleep(1)
+        except(TimeoutException):
+            driver.execute_script('window.stop();')
+            continue
+    print(f'adding {title} to downloads')
+    driver.get(wgetDownload)
     
-def manageRedirects(driver, currentURL, x):
+def manageRedirects(driver, currentURL, x, title):
     if '&r=' in str(currentURL):
         print("request was blocked. make sure you don't have adblock.\nretrying...")
         reloadPage=driver.find_element_by_xpath('//*[@id="mainLoad"]/div/a')
@@ -129,27 +231,26 @@ def manageRedirects(driver, currentURL, x):
         downloadURL=str(currentURL)
         return downloadURL
     else:
-        print('fetching download page...')
-        button=driver.find_element_by_xpath(f'//*[@id="watch_dl"]/table/tbody/tr[{x}]/td[4]/a[1]')
-        ActionChains(driver).click(button).perform()
+        if str(title) in str(currentURL):
+            print('fetching download page...')
+            button=driver.find_element_by_xpath(f'//*[@id="watch_dl"]/table/tbody/tr[{x}]/td[4]/a[1]')
+            ActionChains(driver).click(button).perform()
+        else:
+            print('closing ads...')
+            driver.close()
     return ''
 
-def fetchDownload(driver, downloadURL):
-    wgetDownload=''
-    while wgetDownload=='':
-        time.sleep(1)
-        driver.execute_script("window.stop();")
-        time.sleep(1)
-        currentURL=driver.current_url
-        if str(downloadURL) not in str(currentURL):
-            driver.close()
+def fetchDownload(driver, downloadURL, currentURL):
+    if str(downloadURL) not in str(currentURL):
+        print('closing ads and unnecessary tabs...')
+        driver.close()
+    else:
+        dlButton=driver.find_element_by_xpath('/html/body/div[1]/div/p/a[1]')
+        dlbClass=dlButton.get_attribute('class')
+        if dlbClass=='bigbutton':
+            wgetDownload=dlButton.get_attribute('href')
+            print('found the download URL.')
+            return wgetDownload
         else:
-            dlButton=driver.find_element_by_xpath('/html/body/div[1]/div/p/a[1]')
-            dlbClass=dlButton.get_attribute('class')
-            if dlbClass=='bigbutton':
-                wgetDownload=dlButton.get_attribute('href')
-                return wgetDownload
-            else:
-                ActionChains(driver).click(dlButton).perform()
-        driver.switch_to.window(driver.window_handles[len(driver.window_handles)-1])
-            
+            ActionChains(driver).click(dlButton).perform()
+    return ''
